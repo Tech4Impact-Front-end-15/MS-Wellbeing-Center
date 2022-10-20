c307b9099176c64a8ddc5325b317554109eb9d94
MuhammadZakyR <muhammadzaky.19058@mhs.unesa.ac.id>
2022-10-21 00:13:15 +0700

    Resolve Conflict with List Blog Page

--- src/js/script.js
+++ src/js/script.js
@@ -34,6 +34,25 @@ async function getBlog(){
             </div>
         `
     }
+    let listContainer = document.querySelector("#main")
+
+    for(let i = result.length-9; i < result.length; i++ ){
+        listContainer.innerHTML += 
+        `
+        <div class="p-4 bg-bgSekunder rounded-lg shadow-xl outline-textPrimary outline outline-offset-2 opacity-[95%]">
+            <div class="flex flex-wrap items-center">
+                <img src="${result[i].img}" alt="dummyBg" class="w-full rounded-t-lg h-full sm:h-[200px] lg:h-[300px]">
+                <h3 class="font-semibold text-2xl mt-4 text-center lg:text-left w-full truncate">${result[i].tittle}<span class="lg:text-left block font-semibold text-[12px] text-center lg:text-left">${result[i].author}<span class="font-normal"> | ${result[i].releaseDate}</span></span></h3>
+                <p class="mt-2 truncate-overflow text-left sm:text-justify w-full">
+                ${result[1].descContent[0]}
+                </p>
+                <div class="text-center lg:text-right mt-6 lg:mt-12 lg:mr-4 w-full bg-bgPrimary py-2 rounded-lg lg:bg-bgSekunder lg:py-0 lg:rounded-none hover:opacity-[90%] shadow-lg lg:shadow-none">
+                    <a href="${result[i].link}" target="_blank" class="font-semibold text-textWhite lg:text-textBlack">Readmore<span class="hidden lg:inline">...</span></a>
+                </div>
+            </div>
+        </div>
+        `
+    }
 }
 getBlog()
 
